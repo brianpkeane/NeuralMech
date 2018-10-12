ca48ebfd5d1e69d6c1997a0ba166f181a4a6dc41
Keane Brian <keanebp@amarel1.amarel.rutgers.edu>
2018-10-12 17:20:45 -0400

    Initial commit

---
+++ scripts/subjbatch/mod6-7-restFix-msmall/sub-S31_hcppreprocBatch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --time=14-00:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --partition=p_mc1689_1
+#SBATCH --job-name=Hsub-S31
+#SBATCH --output=slurm.Hsub-S31.out
+#SBATCH --error=slurm.Hsub-S31.err
+#SBATCH --cpus-per-task=4
+#SBATCH --mail-type=END
+#SBATCH --mail-user=brian.keane@rutgers.edu
+#Run the MATLAB command
+cd /home/keanebp/projects/NeuralMech/docs/scripts/
+/home/keanebp/projects/NeuralMech/docs/scripts//Step1_hcp_preprocessing_msmall_amarel.sh --server='amarel' --preFS='false' --FS='false' --postFS='false' --fmriVol='false' --fmriSurf='false' --restFix='true' --msmAll='true' --dedriftResample='false' --subj='sub-S31'
